654e84da04688a9f538d697420e2758a0ec45269
Ahsan Ghani <ghaniahsan@outlook.com>
2021-01-31 22:42:08 -0500

    first and final

---
+++ api/Services/ICartServices.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Services
+{
+    public interface ICartService
+    {
+        double Total();
+        IEnumerable<CartItem> Items();
+    }
+}
---
+++ api/Services/IShipmentServices.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Services
+{
+    public interface IShipmentService
+    {
+        void Ship(IAddressInfo info, IEnumerable<CartItem> items);
+    }
+}
